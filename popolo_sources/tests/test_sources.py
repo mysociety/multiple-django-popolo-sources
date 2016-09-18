16ee6e3c5db76cee8e6dc3bf871b65b0d343efac
Mark Longair <mhl@pobox.com>
2016-09-18 15:58:09 +0100

    Add a test for PopoloSource's __repr__ magic method

--- popolo_sources/tests/test_sources.py
+++ popolo_sources/tests/test_sources.py
@@ -7,3 +7,10 @@ class PopoloSourceTests(TestCase):
 
     def test_object_creation(self):
         PopoloSource.objects.create(url='http://example.com/popolo.json')
+
+    def test_popolo_source_repr(self):
+        ps = PopoloSource.objects.create(url='http://example.com/popolo.json')
+        self.assertEqual(
+            repr(ps),
+            "PopoloSource(id={0}, url='http://example.com/popolo.json')".format(ps.id)
+        )
